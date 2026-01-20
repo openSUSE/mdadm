d128e6b53b68792f4e6bceeb90a770bbd82d1f34
Martin Wilck <mwilck@suse.com>
2026-01-20 17:58:02 +0100

    mdadm: fix compilation errors for unused variables with GCC 16
    In upcoming gcc 16, increments and assignments don't count as "uses",
    causing errors like this:
    
    Assemble.c: In function 'Assemble':
    Assemble.c:1379:55: error: variable 'replcnt' set but not used [-Werror=unused-but-set-variable=]
     1379 |         unsigned int okcnt, sparecnt, rebuilding_cnt, replcnt, journalcnt;
          |                                                       ^~~~~~~
    
    See the porting to for more details: https://gcc.gnu.org/gcc-16/porting_to.html
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>

--- Detail.c
+++ Detail.c
@@ -98,7 +98,6 @@ int Detail(char *dev, struct context *c)
 	int max_devices = 0, n_devices = 0;
 	int spares = 0;
 	struct stat stb;
-	int failed = 0;
 	struct supertype *st = NULL;
 	char *subarray = NULL;
 	int max_disks = MD_SB_DISKS; /* just a default */
@@ -749,12 +748,8 @@ int Detail(char *dev, struct context *c)
 				       disk.raid_disk);
 		}
 		if (!c->brief && array.raid_disks) {
-			if (disk.state & (1 << MD_DISK_FAULTY)) {
+			if (disk.state & (1 << MD_DISK_FAULTY))
 				printf(" faulty");
-				if (disk.raid_disk < array.raid_disks &&
-				    disk.raid_disk >= 0)
-					failed++;
-			}
 			if (disk.state & (1<<MD_DISK_TIMEOUT))
 				printf(" timeout");
 			if (disk.state & (1 << MD_DISK_ACTIVE))
